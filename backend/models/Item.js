334e723fab19259052041a08967e7bf4b88247e3
De Silva R.Y.K IT24104151 <it24104151@my.sliit.lk>
2026-04-28 19:40:10 +0530

    clean init

---
+++ backend/models/Item.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+
+    warrentyTerms: {
+      type: String,
+      required: [true, "warrenty Terms are required"],
+      trim: true,
+    },
+
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
